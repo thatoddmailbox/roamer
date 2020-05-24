1828be17c8ce6002015fcf32754c9ca9155d360f
Alex Studer <alex@studer.dev>
2020-05-24 17:30:38 -0400

    working creation of migrations

--- cli/commandCreate.go
+++ cli/commandCreate.go
@@ -7,5 +7,11 @@ import (
 )
 
 func commandCreate(environment *roamer.Environment, args []string) {
-	fmt.Println("create yay")
+	description := args[0]
+	err := environment.CreateMigration(description)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("A new migration file has been created.")
 }
--- migrations.go
+++ migrations.go
@@ -1,5 +1,13 @@
 package roamer
 
+import (
+	"io/ioutil"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+)
+
 // A Migration represents a distinct operation performed on a database.
 type Migration struct {
 	ID          string
@@ -7,6 +15,27 @@ type Migration struct {
 
 	downFile string
 	upFile   string
+}
+
+func (e *Environment) CreateMigration(description string) error {
+	id := strconv.FormatInt(time.Now().Unix(), 10)
+	normalizedName := strings.Replace(strings.ToLower(description), " ", "_", -1)
+
+	downPath := path.Join(e.fullMigrationsPath, id+"_"+normalizedName+"_down.sql")
+	upPath := path.Join(e.fullMigrationsPath, id+"_"+normalizedName+"_up.sql")
+
+	contents := "-- Description: " + description + "\n-- "
+
+	err := ioutil.WriteFile(downPath, []byte(contents+"Down migration\n\n"), 0777)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(upPath, []byte(contents+"Up migration\n\n"), 0777)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (e *Environment) ListAllMigrations() ([]Migration, error) {
